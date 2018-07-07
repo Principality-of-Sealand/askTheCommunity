f7f633eb7fe77947471e7a9e328cd0ebc3de5195
Jamison <jlevitin@gmail.com>
2018-07-06 17:15:17 -0700

    Modified the database for deployment, commented out the parts only needed for creating the initial DB, tables & indexes

--- database/schemas.js
+++ database/schemas.js
@@ -3,16 +3,16 @@ const copyFrom = require('pg-copy-streams').from;
 const fs = require('fs');
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: "18.191.60.178",
   database: 'practice',
-  password: 'root',
+  password: process.env.DB,
   port: '5432'
 })
 const client = new Client({
   user: 'postgres',
-  host: 'localhost',
+  host: "18.191.60.178",
   database: 'postgres',
-  password: 'root',
+  password: process.env.DB,
   port: '5432'
 })
 //  only for recreating the database
@@ -29,42 +29,74 @@ const client = new Client({
       if(err) {
         console.log('unable to connect to created db', err)
       }
-      newClient.query(`DROP TABLE IF EXISTS "questions"`)
-      newClient.query(`CREATE TABLE IF NOT EXISTS questions (
-          ID SERIAL,
-          USER_ID INTEGER,
-          RESTAURANT_ID INTEGER,
-          TEXT VARCHAR(255),
-          PARENT_ID INTEGER,
-          HELPFUL INTEGER,
-          ANSWER TEXT,
-          "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-          "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-          PRIMARY KEY (ID)
-      );`
-    )
-    let stream = newClient.query(copyFrom(`COPY questions (user_id, restaurant_id, text, parent_id, helpful, "createdAt", "updatedAt") FROM STDIN WITH NULL AS '@';`));
-    let fileStream = fs.createReadStream('./questions.csv')
-    fileStream.on('error', done);
-    stream.on('error', done);
-    stream.on('end', done);
-    fileStream.pipe(stream);
-    newClient.query(`DROP TABLE IF EXISTS users`)
-    newClient.query(`CREATE TABLE users (
-      ID  SERIAL,
-      USERNAME VARCHAR(255),
-      "imageUrl" TEXT,
-      "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-      "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-      PRIMARY KEY (ID)
-    );`
-  )
-  let streamUsers = newClient.query(copyFrom(`COPY users (username, "imageUrl", "createdAt", "updatedAt") FROM STDIN;`));
-  let fileStreamUsers = fs.createReadStream('./users.csv')
-  fileStreamUsers.on('error', done);
-  streamUsers.on('error', done);
-  streamUsers.on('end', done);
-  fileStreamUsers.pipe(streamUsers);
+      console.log('connected to db')
+    //   newClient.query(`CREATE TABLE IF NOT EXISTS questions (
+    //       ID SERIAL,
+    //       USER_ID INTEGER,
+    //       RESTAURANT_ID INTEGER,
+    //       TEXT VARCHAR(255),
+    //       PARENT_ID INTEGER,
+    //       HELPFUL INTEGER,
+    //       ANSWER TEXT,
+    //       "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+    //       "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+    //       PRIMARY KEY (ID)
+    //   );`
+    // )
+    // .then(table => {
+    //   console.log('questions table created', table)
+    // })
+    // .catch(err => {
+    //   console.log('error creating questions table', err)
+    // })
+    // newClient.query(`CREATE INDEX restaurant_id on questions(restaurant_id)`)
+    // .then(index => {
+    //   console.log('Index restaurant_id created')
+    // })
+    // .catch(err => {
+    //   console.log('error creating the index', err)
+    // })
+    // newClient.query('CREATE INDEX parent_id on questions(parent_id)')
+    //   .then(index => {
+    //     console.log('Index parent_id created')
+    //   })
+    //   .catch(err => {
+    //     console.log('error creating the index', err)
+    //   })
+  //   let stream = newClient.query(copyFrom(`COPY questions (user_id, restaurant_id, text, parent_id, helpful, "createdAt", "updatedAt") FROM STDIN WITH NULL AS '@';`));
+  //   let fileStream = fs.createReadStream('./questions.csv')
+  //   fileStream.on('error', done);
+  //   stream.on('error', done);
+  //   stream.on('end', done);
+  //   fileStream.pipe(stream);
+  //   newClient.query(`CREATE TABLE IF NOT EXISTS users (
+  //     ID  SERIAL,
+  //     USERNAME VARCHAR(255),
+  //     "imageUrl" TEXT,
+  //     "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+  //     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+  //     PRIMARY KEY (ID)
+  //   );`
+  // )
+  // .then(table => {
+  //   console.log('users table created', table)
+  // })
+  // .catch(err => {
+  //   console.log('error creating users table', err)
+  // })
+  // newClient.query(`CREATE INDEX id on users(id)`)
+  // .then(index => {
+  //   console.log('INDEX id created')
+  // })
+  // .catch(err => {
+  //   console.log('error creating the index', err)
+  // })
+  // let streamUsers = newClient.query(copyFrom(`COPY users (username, "imageUrl", "createdAt", "updatedAt") FROM STDIN;`));
+  // let fileStreamUsers = fs.createReadStream('./users.csv')
+  // fileStreamUsers.on('error', done);
+  // streamUsers.on('error', done);
+  // streamUsers.on('end', done);
+  // fileStreamUsers.pipe(streamUsers);
   })
 
   module.exports = {pool:pool}
